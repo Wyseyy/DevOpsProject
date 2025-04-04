4430a58e0eea6d9f25c6c19e487ef61c222a53c4
Wyseyy <eoinwysey@gmail.com>
2025-04-04 12:02:33 +0100

    index page updates

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-
+const databaseRecords = () =>{
+  return [
+    { id: 1, name: "Channel Orange", artist: "Frank Ocean", releaseYear: 2012, inStock: false},
+    { id: 2, name: "Brand New Eyes", artist: "Paramore", releaseYear: 2009, inStock: true},
+    {id: 3, name: "Igor", artist: "Tyler The Creator", releaseYear: 2019, inStock: true},
+  ]
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "DevOps Projects" });
+  const records = getRecords();//fetch records from database
+  res.render('index', { title: "DevOps Projects", records });
 });
 
 
