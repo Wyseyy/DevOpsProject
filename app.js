903f947607223dc8abd9848035832c5def9d78f5
Your Name <my@email.org>
2025-04-04 16:12:59 +0100

    add read.ejs to render records

--- app.js
+++ app.js
@@ -8,6 +8,53 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+// filename - index.js 
+const sqlite3 = require('sqlite3');
+
+// Connecting Database
+let db = new sqlite3.Database(":memory:" , (err) => {
+    if(err)
+    {
+        console.log("Error Occurred - " + err.message);
+    }
+    else
+    {
+        console.log("DataBase Connected");
+    }
+})
+
+const createTableSql = `
+    CREATE TABLE IF NOT EXISTS records (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        record_name TEXT NOT NULL,
+        record_artist TEXT UNIQUE NOT NULL,
+        release_date TEXT NOT NULL,
+        in_stock BOOLEAN
+    )`;
+    
+    app.get("/", (req, res) => {
+        db.all("SELECT * FROM records", [], (err, rows) => {
+            if (err) {
+                return res.status(500).send("Error fetching records");
+            }
+            // Render the EJS view with the records data
+            res.render("read", { records: rows });
+        });
+    });
+
+// Execute the SQL statement to create the table
+db.run(createTableSql, (err) => {
+    if (err) {
+        return console.error('Error creating table:', err.message);
+    }
+    console.log('Table created successfully');
+});
+
+// Server Running
+app.listen(4000 , () => {
+    console.log("Server started");
+})
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,7 +65,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var indexRouter = require('./routes/index')
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
