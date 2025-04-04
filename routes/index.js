df28e640f1ed562d2e2890305ef90d750ee02019
Your Name <my@email.org>
2025-04-04 15:55:22 +0100

    add server set-up to index remove server from app js as app js contains configuration

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
+// filename - index.js 
+const sqlite3 = require('sqlite3');
+
+// Connecting Database
+let db = new sqlite3.Database(":memory:" , (err) => {
+    if(err)
+    {
+        console.log("Error Occurred - " + err.message);
+    }
+    else
+    {
+        console.log("DataBase Connected");
+    }
+})
+
+const createTableSql = `
+    CREATE TABLE IF NOT EXISTS records (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        record_name TEXT NOT NULL,
+        record_artist TEXT UNIQUE NOT NULL,
+        release_date TEXT NOT NULL,
+        in_stock BOOLEAN
+    )`;
+    
+    app.get("/", (req, res) => {
+        db.all("SELECT * FROM records", [], (err, rows) => {
+            if (err) {
+                return res.status(500).send("Error fetching records");
+            }
+            // Render the EJS view with the records data
+            res.render("read", { records: rows });
+        });
+    });
+
+// Execute the SQL statement to create the table
+db.run(createTableSql, (err) => {
+    if (err) {
+        return console.error('Error creating table:', err.message);
+    }
+    console.log('Table created successfully');
+});
+
 const databaseRecords = () =>{
   return [
     { id: 1, name: "Channel Orange", artist: "Frank Ocean", releaseYear: 2012, InStock: false},
@@ -8,6 +50,12 @@ const databaseRecords = () =>{
     {id: 3, name: "Igor", artist: "Tyler The Creator", releaseYear: 2019, InStock: true},
   ]
 }
+
+// Server Running
+app.listen(4000 , () => {
+    console.log("Server started");
+})
+
 /* Home page (brings to read page). */
 router.get('/read', function(req, res, next) {
   res.render('read', { title: "View records"});
